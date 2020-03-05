3842c5f50460da35e29bd690fee6b095cade6a10
vjkancherla <vijay.kancherla@rackspace.co.uk>
2020-03-05 15:48:59 +0000

    p6

--- Project-6/Jenkins-Pipeline_Terrafrom_Ansible_Wordpress/Ansible/wordpress-nginx/run.sh
+++ Project-6/Jenkins-Pipeline_Terrafrom_Ansible_Wordpress/Ansible/wordpress-nginx/run.sh
@@ -7,4 +7,4 @@ jenk_proj_ws=$1
 echo "sleeping for 2 mins for the DB and Web instances to comeup"
 sleep 120
 
-ansible-playbook -i ./ec2.py site.yml --extra-vars 'jenkins_project_workspace=${jenk_proj_ws}'
+ansible-playbook -i ./ec2.py site.yml --extra-vars jenkins_project_workspace=${jenk_proj_ws}
