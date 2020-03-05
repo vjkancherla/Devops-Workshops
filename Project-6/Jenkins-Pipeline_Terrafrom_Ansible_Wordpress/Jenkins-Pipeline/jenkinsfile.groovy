315c620727f79f3054c34f5d3cb2d6b0ce16ce15
vjkancherla <vijay.kancherla@rackspace.co.uk>
2020-03-05 16:11:52 +0000

    p6

--- Project-6/Jenkins-Pipeline_Terrafrom_Ansible_Wordpress/Jenkins-Pipeline/jenkinsfile.groovy
+++ Project-6/Jenkins-Pipeline_Terrafrom_Ansible_Wordpress/Jenkins-Pipeline/jenkinsfile.groovy
@@ -18,6 +18,14 @@ pipeline {
             steps { provisionWordPress() }
 		}
 
+        stage("Approve-Infra-Teardown") {
+            steps { approveTeardown() }
+    }
+
+        stage("Teardown-WordPress-Infra") {
+            steps { tearDownInfra() }
+    }
+
 	}
 }
 
@@ -35,5 +43,18 @@ def provisionWordPress() {
     sh "chmod 500 ssh_keys/*"
     sh "chmod +x ec2.py run.sh"
     sh "./run.sh /var/lib/jenkins/workspace/WP-pipeline"
+	}
+}
+
+def approveTeardown() {
+  timeout(time:1, unit:'DAYS') {
+		input('Are you sure you want to teardown the WordPress Infra?')
+	}
+}
+
+def tearDownInfra() {
+  dir ('Project-6/Jenkins-Pipeline_Terrafrom_Ansible_Wordpress/Terraform/layers/wordpress' ) {
+    sh "chmod +x destroy-infra.sh"
+    sh "./destroy-infra.sh"
 	}
 }
--- Project-6/Jenkins-Pipeline_Terrafrom_Ansible_Wordpress/Terraform/layers/wordpress/main.tf
+++ Project-6/Jenkins-Pipeline_Terrafrom_Ansible_Wordpress/Terraform/layers/wordpress/main.tf
@@ -147,3 +147,13 @@ resource "aws_security_group" "wordpress-ec2-sg" {
     cidr_blocks     = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group_rule" "allow_all" {
+  type            = "ingress"
+  from_port       = 0
+  to_port         = 65535
+  protocol        = "tcp"
+  source_security_group_id = "${aws_security_group.wordpress-ec2-sg.id}"
+  security_group_id = "${aws_security_group.wordpress-ec2-sg.id}"
+  description = "allow all traffic within the SG"
+}
