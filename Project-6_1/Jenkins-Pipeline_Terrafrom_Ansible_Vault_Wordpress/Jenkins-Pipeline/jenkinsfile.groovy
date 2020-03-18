74ede2dedb1b87c343c8cc7d05a9d1cea2055a19
vjkancherla <vijay.kancherla@rackspace.co.uk>
2020-03-18 14:35:01 +0530

    Project 6_1 14:34

--- Project-6_1/Jenkins-Pipeline_Terrafrom_Ansible_Vault_Wordpress/Jenkins-Pipeline/jenkinsfile.groovy
+++ Project-6_1/Jenkins-Pipeline_Terrafrom_Ansible_Vault_Wordpress/Jenkins-Pipeline/jenkinsfile.groovy
@@ -41,14 +41,14 @@ pipeline {
 
 // steps
 def buildInfra() {
-	dir ('${workspace_path}/Terraform/layers/wordpress' ) {
+	dir ('${env.workspace_path}/Terraform/layers/wordpress' ) {
     sh "chmod +x create-infra.sh"
     sh "./create-infra.sh"
 	}
 }
 
 def provisionWordPress() {
-  dir ('${workspace_path}/Ansible/wordpress-nginx' ) {
+  dir ('${env.workspace_path}/Ansible/wordpress-nginx' ) {
     sh "chmod +x ec2.py run.sh"
     sh "./run.sh"
 	}
@@ -61,7 +61,7 @@ def approveTeardown() {
 }
 
 def tearDownInfra() {
-  dir ('${workspace_path}/Terraform/layers/wordpress' ) {
+  dir ('${env.workspace_path}/Terraform/layers/wordpress' ) {
     sh "chmod +x destroy-infra.sh"
     sh "./destroy-infra.sh"
 	}
